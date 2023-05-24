4ab6c4f460a422a9f257af11e5d31fbb3fa9e6a2
Joakim Villo <villokodehode@gmail.com>
2023-05-24 18:15:32 +0200

    fixed bottom CTA

--- components/base components/Icon.jsx
+++ components/base components/Icon.jsx
@@ -5,10 +5,28 @@ export default function VilloIcon() {
   return (
     <div className="w-8 h-8">
       <Image
-        src="/logo_main_shape.svg"
+        src="/logo/WindLogoNoText.svg"
         className="w-auto h-full duration-500 hover:rotate-45"
         width={15}
         height={15}
+        alt=""
+      />
+    </div>
+
+    // <div
+    //   className={`animate-Appear h-6 w-6 border-l-8 border-t-8 border-r-8 border-r-JWC-tertiary border-l-JWC-primary border-t-JWC-secondary duration-75 z-10 rounded-full `}
+    // ></div>
+  );
+}
+
+export function VilloIconAbsoluted() {
+  return (
+    <div className="absolute w-56 h-56 -translate-y-1/2">
+      <Image
+        src="/logo/WindLogoNoText.svg"
+        className="w-full h-full duration-500 hover:rotate-45"
+        width={150}
+        height={150}
         alt=""
       />
     </div>
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,9 @@ import ResponsiveH1, {
 } from "@/components/Responsive text/ResponsiveText";
 import Link from "next/link";
 
-import JWCIcon from "@/components/base components/Icon";
+import VilloIcon, {
+  VilloIconAbsoluted,
+} from "@/components/base components/Icon";
 import { useContext, useState } from "react";
 import THREESpace from "@/components/animation/Space";
 import LanguageContext from "@/components/Utilities/LanguageSwitch/LanguageContext";
@@ -46,20 +48,27 @@ export default function Home() {
         <BenefitsSection />
         <ServicesSection />
 
-        <section className=" relative grid items-center justify-center gap-2 px-12 pt-12 mx-auto md:py-12 sm:pt-0 xl:gap-16 mb-[116px]">
-          <TextFlex>
-            <ResponsiveH3 className="flex px-12 text-center bg-gradient-to-r from-JWC-tertiary to-JWC-secondary bg-clip-text transparent">
+        <section className="relative grid items-center justify-center gap-2 px-12 pt-12 mx-auto md:py-12 sm:pt-0 xl:gap-16 mb-[116px]">
+          <div className="z-20">
+            <ResponsiveH3 className="z-20 flex px-12 text-center bg-gradient-to-r from-Villo-tertiary to-Villo-secondary bg-clip-text transparent">
               {language === "Norwegian"
                 ? "La oss komme i gang!"
                 : "Let's get started!"}
             </ResponsiveH3>
-          </TextFlex>
-          <div className="flex flex-col items-center justify-center w-full">
+          </div>
+          <div className="relative z-20 flex flex-col items-center justify-center group">
             <Link href="/contact">
               <CallToActionButton>
                 {language === "Norwegian" ? "Ta kontakt" : "Contact me"}
               </CallToActionButton>
             </Link>
+            <Image
+              src="/logo/WindLogoNoText.svg"
+              className="absolute w-48 h-48 duration-500 -translate-y-1/2 -z-10 group-active:scale-110 group-hover:rotate-180 group-hover:scale-150 top-1/2"
+              width={150}
+              height={150}
+              alt=""
+            />
           </div>
         </section>
       </PageLayout>
