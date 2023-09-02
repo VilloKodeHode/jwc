47e2f70c761867842c7fdfaec6938f53b3487886
Joakim Villo <villokodehode@gmail.com>
2023-09-02 10:35:31 +0200

    fixed hover effect BenefitsSec

--- components/sections/sectionsParts/BenefitsSectionParts.jsx
+++ components/sections/sectionsParts/BenefitsSectionParts.jsx
@@ -15,39 +15,38 @@ export const BenefitsSectionPart = ({
 }) => {
   return (
     <div className="mt-5 transition-all duration-300 md:mt-0 md:col-span-1 hover:scale-[1.02] active:scale-[1.02] group cursor-default">
-      <div className="overflow-hidden shadow sm:rounded-lg">
+      <div className={`overflow-hidden transition-all duration-300 shadow sm:rounded-lg group-hover:outline  ${Theme === "light"
+        ? " group-hover:outline-Villo-light-white20"
+        : " group-hover:outline-Villo-dark-black50"
+        }`}>
         <div
-          className={`min-h-[131px] transition-colors duration-300 px-4 py-5  ${
-            Theme === "light"
-              ? "bg-Villo-light-white10 group-hover:bg-Villo-light-primary"
-              : "bg-Villo-dark-black75 group-hover:bg-Villo-dark-primary"
-          } sm:p-6`}
+          className={`min-h-[131px] transition-colors duration-300 px-4 py-5  ${Theme === "light"
+            ? "bg-Villo-light-white10 group-hover:bg-Villo-light-primary"
+            : "bg-Villo-dark-black75 group-hover:bg-Villo-dark-primary"
+            } sm:p-6`}
         >
           <ResponsiveThemedH5
-            className={`font-bold max-w-fit ${
-              Theme === "light"
-                ? "group-hover:text-Villo-light-white"
-                : "group-hover:text-Villo-dark-black"
-            }`}
+            className={`font-bold max-w-fit ${Theme === "light"
+              ? "group-hover:text-Villo-light-white"
+              : "group-hover:text-Villo-dark-black"
+              }`}
           >
             {language === "Norwegian" ? norTitle : engTitle}
           </ResponsiveThemedH5>
           <ResponsiveThemedH5
-            className={`font-bold max-w-fit ${
-              Theme === "light"
-                ? "group-hover:text-Villo-light-white"
-                : "group-hover:text-Villo-dark-black"
-            }`}
+            className={`font-bold max-w-fit ${Theme === "light"
+              ? "group-hover:text-Villo-light-white"
+              : "group-hover:text-Villo-dark-black"
+              }`}
           >
             {language === "Norwegian" ? norSubTitle : engSubTitle}
           </ResponsiveThemedH5>
           <div className="mt-2">
             <ThemedP
-              className={`${
-                Theme === "light"
-                  ? "group-hover:text-Villo-light-white"
-                  : "group-hover:text-Villo-dark-black"
-              }`}
+              className={`${Theme === "light"
+                ? "group-hover:text-Villo-light-white"
+                : "group-hover:text-Villo-dark-black"
+                }`}
             >
               {language === "Norwegian" ? norText : engText}
             </ThemedP>
