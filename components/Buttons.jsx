a051f85b477720b5b9ffee244e57ed4621fc22f4
Joakim Villo <villokodehode@gmail.com>
2023-10-18 23:17:52 +0200

    minor adjustment to button colors

--- components/Buttons.jsx
+++ components/Buttons.jsx
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
-import {
-  ResponsiveH5,
-  ResponsiveP,
-  ResponsiveThemedH5,
-} from "./Responsive text/ResponsiveText";
+import { ResponsiveH5 } from "./Responsive text/ResponsiveText";
 import {
   FiArrowRight,
   FiArrowUpCircle,
@@ -11,8 +7,7 @@ import {
   FiSend,
 } from "react-icons/fi";
 import { TfiEmail } from "react-icons/tfi";
-
-import { MdDoubleArrow } from "react-icons/md";
+import { MdDoubleArrow, MdOutlineWorkOutline } from "react-icons/md";
 
 export default function CallToActionButton({
   children,
@@ -58,7 +53,7 @@ export function ArrowCTA({ children, Theme, onClick, className }) {
     <button
       onClick={onClick}
       className={`relative px-6 pr-12 outline outline-2 flex justify-center items-center group hover:outline-none py-4 my-4 font-bold uppercase transition duration-200 ease-in-out ${className} ${Theme === "light"
-        ? "text-Villo-light-black85 bg-Villo-light-white outline-Villo-light-black85 hover:text-Villo-light-white hover:bg-Villo-dark-secondary"
+        ? "text-Villo-light-black85 bg-Villo-light-white outline-Villo-light-black85 hover:text-Villo-light-white hover:bg-Villo-light-primary"
         : "text-Villo-dark-white10 bg-Villo-light-black outline-Villo-dark-white10 hover:bg-Villo-dark-primary"
         } rounded-full shadow-md xl:w-fit active:scale-95 text-p leading-p hover:scale-[1.05] origin-center`}
     >
@@ -99,8 +94,8 @@ export function ExternalCTA({ children, Theme, onClick, className }) {
     <button
       onClick={onClick}
       className={`px-6 outline outline-2 flex justify-center items-center group hover:outline-none py-4 my-4 font-bold uppercase transition duration-100 ease-in-out ${className} ${Theme === "light"
-        ? "text-Villo-light-black85 bg-Villo-light-white outline-Villo-light-black85 hover:text-Villo-light-white hover:bg-Villo-dark-primary"
-        : "text-Villo-dark-white10 bg-Villo-light-black outline-Villo-dark-white10 hover:bg-Villo-light-primary"
+        ? "text-Villo-light-black85 bg-Villo-light-white outline-Villo-light-black85 hover:text-Villo-light-white hover:bg-Villo-light-primary"
+        : "text-Villo-dark-white10 bg-Villo-light-black outline-Villo-dark-white10 hover:bg-Villo-dark-primary"
         } rounded-full shadow-md xl:w-fit active:scale-95 text-p leading-p hover:scale-[1.02]`}
     >
       <>
@@ -116,8 +111,8 @@ export function SendCTA({ children, Theme, onClick, className }) {
     <button
       onClick={onClick}
       className={`px-6 outline outline-2 flex justify-center items-center group hover:outline-none py-4 my-4 font-bold uppercase transition duration-100 ease-in-out ${className} ${Theme === "light"
-        ? "text-Villo-light-black85 bg-Villo-light-white outline-Villo-light-black85 hover:text-Villo-light-white hover:bg-Villo-dark-primary"
-        : "text-Villo-dark-white10 bg-Villo-light-black outline-Villo-dark-white10 hover:bg-Villo-light-primary"
+        ? "text-Villo-light-black85 bg-Villo-light-white outline-Villo-light-black85 hover:text-Villo-light-white hover:bg-Villo-light-primary"
+        : "text-Villo-dark-white10 bg-Villo-light-black outline-Villo-dark-white10 hover:bg-Villo-dark-primary"
         } rounded-full shadow-md xl:w-fit active:scale-95 text-p leading-p hover:scale-[1.02]`}
     >
       <>
@@ -145,6 +140,23 @@ export function EmailCTA({ children, Theme, onClick, className }) {
   );
 }
 
+export function PortfolioCTA({ children, Theme, onClick, className }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-6 outline outline-2 flex justify-center shadow items-center group hover:outline-none py-4 my-4 font-bold uppercase transition duration-100 ease-in-out ${className} ${Theme === "light"
+        ? "text-Villo-light-black85 bg-Villo-light-white hover:shadow-Villo-light-black outline-Villo-light-black85 hover:text-Villo-light-white hover:bg-Villo-light-primary"
+        : "text-Villo-dark-white10 bg-Villo-light-black outline-Villo-dark-white10 hover:shadow-Villo-dark-black hover:bg-Villo-dark-primary"
+        } rounded-full shadow-md xl:w-fit active:scale-95 text-p leading-p hover:scale-[1.02]`}
+    >
+      <>
+        {children}
+        <MdOutlineWorkOutline className="inline-block w-5 h-5 m-auto ml-2 transition-all duration-200 group-hover:-translate-y-1/2 group-hover:animate-cookieShake opacity-90 group-hover:opacity-100 group-hover:ml-2 group-hover:w-5 group-hover:h-5" />
+      </>
+    </button>
+  );
+}
+
 
 
 export function CookieAccept({ children, Theme, onClick, className }) {
@@ -152,8 +164,8 @@ export function CookieAccept({ children, Theme, onClick, className }) {
     <button
       onClick={onClick}
       className={`px-6 outline outline-2 group hover:outline-none py-2 font-bold uppercase transition duration-200 ease-in-out ${className} ${Theme === "light"
-        ? "text-Villo-light-black85 bg-Villo-light-white outline-Villo-light-black85 hover:text-Villo-light-white  hover:bg-Villo-dark-primary"
-        : "text-Villo-dark-white10 bg-Villo-light-black outline-Villo-dark-white10 hover:bg-Villo-light-primary"
+        ? "text-Villo-light-black85 bg-Villo-light-white outline-Villo-light-black85 hover:text-Villo-light-white  hover:bg-Villo-light-primary"
+        : "text-Villo-dark-white10 bg-Villo-light-black outline-Villo-dark-white10 hover:bg-Villo-dark-primary"
         } rounded-full shadow-md xl:w-fit active:scale-95 text-p leading-p hover:scale-[1.02]`}
     >
       <>{children}</>
@@ -166,8 +178,8 @@ export function ToolsToggleButton({ children, Theme, onClick, className }) {
     <button
       onClick={onClick}
       className={`outline outline-2 flex justify-center items-center group hover:outline-none py-2 px-4 pl-8 my-4 font-bold uppercase transition-all duration-200 ease-in-out ${className} ${Theme === "light"
-        ? "text-Villo-light-black85 bg-Villo-light-white outline-Villo-light-black85 hover:text-Villo-light-white hover:bg-Villo-light-secondary"
-        : "text-Villo-dark-white10 bg-Villo-light-black outline-Villo-dark-white10 hover:bg-Villo-light-primary"
+        ? "text-Villo-light-black85 bg-Villo-light-white outline-Villo-light-black85 hover:text-Villo-light-white hover:bg-Villo-light-primary"
+        : "text-Villo-dark-white10 bg-Villo-light-black outline-Villo-dark-white10 hover:bg-Villo-dark-primary"
         } rounded-full shadow-md xl:w-fit active:scale-95 text-p leading-p hover:scale-[1.05] origin-bottom-right`}
     >
       <>
