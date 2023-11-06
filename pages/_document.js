9e4523ab5aea4c9103b1d9fc3cb5c7d05d960433
Joakim Villo <villokodehode@gmail.com>
2023-11-06 20:33:51 +0100

    *

--- pages/_document.js
+++ pages/_document.js
@@ -7,16 +7,18 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      <Head />
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-METSFG26LN" />
-      <Script id="google-analytics">
-        {`
+      <Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-METSFG26LN" />
+        <Script id="google-analytics">
+          {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
           gtag('config', 'G-METSFG26LN');
         `}
-      </Script>
+        </Script>
+      </Head>
+
       <body className={`scrollbar scrollbar-track-Villo-dark-black75 scrollbar-thumb-Villo-light-white20`}>
         <Main />
         <NextScript />
