1b970219ba83d2cf7457ba5b4def55c1e92815df
Joakim Villo <villokodehode@gmail.com>
2023-11-06 20:38:10 +0100

    *

--- components/Utilities/Metatags.js
+++ components/Utilities/Metatags.js
@@ -1,6 +1,7 @@
 // components/MetaTags.js
 
 import Head from "next/head";
+import Script from "next/script";
 
 function MetaTags({ title, description, url, Theme }) {
   return (
@@ -33,6 +34,18 @@ function MetaTags({ title, description, url, Theme }) {
             : "/logo/WindLogoNoTextDarkMode.svg"
         }
       />
+
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-METSFG26LN" />
+      <script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-METSFG26LN');
+        `}
+      </script>
+
+
     </Head>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -7,18 +7,7 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      <Head>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-METSFG26LN" />
-        <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-          gtag('config', 'G-METSFG26LN');
-        `}
-        </Script>
-      </Head>
-
+      <Head />
       <body className={`scrollbar scrollbar-track-Villo-dark-black75 scrollbar-thumb-Villo-light-white20`}>
         <Main />
         <NextScript />
