d1c7b69b1088d85be0fe8db7e83910fbf881bace
Joakim Villo <villokodehode@gmail.com>
2023-11-27 23:52:32 +0100

    added tooltips and started on glow

--- components/sections/AboutPage/SkillsSection.jsx
+++ components/sections/AboutPage/SkillsSection.jsx
@@ -50,148 +50,235 @@ export const SkillsSection = ({ language, Theme }) => {
           } `}
         >
           <div
-            className={`flex flex-wrap transition-colors duration-1000    md:gap-20 gap-12`}
+            className={`flex flex-wrap justify-center transition-colors duration-1000 md:gap-24 gap-12`}
           >
-            <Image
-              src="/images/skills/html-logo.png"
-              className="relative z-10 w-[53.5px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="react logo"
-              height={60}
-              width={60}
-            ></Image>
-
-            <Image
-              src="/images/skills/react-logo.png"
-              className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="react logo"
-              height={60}
-              width={60}
-            ></Image>
-
-            <Image
-              src="/images/skills/css-logo.png"
-              className="relative z-10 w-[53.5px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="react logo"
-              height={60}
-              width={60}
-            ></Image>
-            <Image
-              src="/images/skills/javascript-logo.png"
-              className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="javascript logo"
-              height={60}
-              width={60}
-            ></Image>
-            <Image
-              src="/images/skills/nextjs-logo.png"
-              className="relative z-10 w-[60px] aspect-auto m-auto duration-500 contrast-50 hover:contrast-100 hover:scale-110"
-              alt="nextjs logo"
-              height={60}
-              width={60}
-            ></Image>
-            <Image
-              src="/images/skills/svelte-logo.png"
-              className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="nextjs logo"
-              height={60}
-              width={60}
-            ></Image>
-            <Image
-              src="/images/skills/nuxt-logo.png"
-              className="relative z-10 w-[92px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="nextjs logo"
-              height={60}
-              width={60}
-            ></Image>
-            <Image
-              src="/images/skills/vsc.png"
-              className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="react logo"
-              height={60}
-              width={60}
-            ></Image>
-            <Image
-              src="/images/skills/git-logo.png"
-              className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="nextjs logo"
-              height={60}
-              width={60}
-            ></Image>
-            <Image
-              src="/images/skills/github-logo.png"
-              className="relative z-10 w-[60px] aspect-auto m-auto duration-500 contrast-50 hover:contrast-100 hover:scale-110"
-              alt="nextjs logo"
-              height={60}
-              width={60}
-            ></Image>
-            <Image
-              src="/images/skills/tailwindcss-logo.png"
-              className="relative object-fit z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="figma logo"
-              height={80}
-              width={80}
-            />
-
-            <figure className="grid grid-cols-[2fr_1fr] w-[120px] group hover:scale-110 duration-500">
+            <figure className="relative group h-[60px] flex justify-center">
               <Image
-                src="/images/skills/daisy1-logo.png"
-                className="relative object-fit z-10 w-[70px] aspect-auto m-auto duration-500 saturate-[70%] group-hover:saturate-100"
-                alt="figma logo"
-                height={80}
-                width={80}
+                src="/images/skills/html-logo.png"
+                className="relative z-10 w-[53.5px] aspect-auto m-auto duration-500 saturate-[70%] group-hover:saturate-100 group-hover:scale-125"
+                alt="html logo"
+                height={60}
+                width={60}
               />
-              <Image
-                src="/images/skills/daisy2-logo.png"
-                className="relative object-fit z-10 w-[35px] aspect-auto my-auto duration-500 contrast-50 group-hover:contrast-100"
-                alt="figma logo"
-                height={80}
-                width={80}
-              />
+              <div className="absolute group-hover:scale-125 duration-500 rounded-full shadow-[0_0_20px_30px_#e15021] translate-x-1/2 translate-y-1/2  bottom-1/2 right-1/2" />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Html</ThemedP>
+              </span>
             </figure>
-            <Image
-              src="/images/skills/chakraUI-logo.png"
-              className="relative object-fit z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="figma logo"
-              height={80}
-              width={80}
-            />
-
-            <Image
-              src="/images/skills/figma-logo.png"
-              className="relative object-fit z-10 w-[40px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="figma logo"
-              height={80}
-              width={80}
-            />
-
-            <Image
-              src="/images/skills/canva-logo.png"
-              className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="react logo"
-              height={60}
-              width={60}
-            />
-            <Image
-              src="/images/skills/wix-logo.png"
-              className="relative z-10 w-[120px] aspect-auto m-auto duration-500 contrast-50 hover:contrast-100 hover:scale-110"
-              alt="react logo"
-              height={120}
-              width={120}
-            ></Image>
-            <Image
-              src="/images/skills/squarespace-logo.png"
-              className="relative z-10 w-[60px] aspect-auto m-auto duration-500 contrast-50 hover:contrast-100 hover:scale-110"
-              alt="react logo"
-              height={60}
-              width={60}
-            />
-            <Image
-              src="/images/skills/sanity-logo.png"
-              className="relative z-10 w-[45px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-110"
-              alt="react logo"
-              height={60}
-              width={60}
-            />
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/react-logo.png"
+                className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="react logo"
+                height={60}
+                width={60}
+              />
+              <div className="absolute group-hover:scale-125 duration-500  shadow-[0_0px_30px_10px_#00d8fe]  translate-x-1/2 translate-y-1/2  bottom-1/2 right-1/2" />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>React</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/css-logo.png"
+                className="relative z-10 w-[53.5px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="css logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Css</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/javascript-logo.png"
+                className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="javascript logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Javascript</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/nextjs-logo.png"
+                className="relative z-10 w-[60px] aspect-auto m-auto duration-500 contrast-50 hover:contrast-100 hover:scale-125"
+                alt="nextjs logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Next.js</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/svelte-logo.png"
+                className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="svelte logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Svelte</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/nuxt-logo.png"
+                className="relative z-10 w-[92px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="nuxt logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Nuxt.js</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/vsc.png"
+                className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="vsc logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold text-center duration-500 translate-x-1/2 opacity-0 select-none w-max group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Visual Studio Code</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/git-logo.png"
+                className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="git logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Git</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/github-logo.png"
+                className="relative z-10 w-[60px] aspect-auto m-auto duration-500 contrast-50 hover:contrast-100 hover:scale-125"
+                alt="github logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Github</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/tailwindcss-logo.png"
+                className="relative object-fit z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="tailwind logo"
+                height={80}
+                width={80}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none w-max group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Tailwind css</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <div className="grid grid-cols-[2fr_1fr] w-[120px] group-hover:scale-125 duration-500">
+                <Image
+                  src="/images/skills/daisy1-logo.png"
+                  className="relative object-fit z-10 w-[70px] aspect-auto m-auto duration-500 saturate-[70%] group-hover:saturate-100"
+                  alt="daisy logo - daisy part"
+                  height={80}
+                  width={80}
+                />
+                <Image
+                  src="/images/skills/daisy2-logo.png"
+                  className="relative object-fit z-10 w-[35px] aspect-auto my-auto duration-500 contrast-50 group-hover:contrast-100"
+                  alt="daisyUI logo - UI part"
+                  height={80}
+                  width={80}
+                />
+              </div>
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none w-max group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>DaisyUI</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/chakraUI-logo.png"
+                className="relative object-fit z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="figma logo"
+                height={80}
+                width={80}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none w-max group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>ChakraUI</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/figma-logo.png"
+                className="relative object-fit z-10 w-[40px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="figma logo"
+                height={80}
+                width={80}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none w-max group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Figma</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/canva-logo.png"
+                className="relative z-10 w-[60px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="react logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none w-max group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Canva</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/wix-logo.png"
+                className="relative z-10 w-[120px] aspect-auto m-auto duration-500 contrast-50 hover:contrast-100 hover:scale-125"
+                alt="react logo"
+                height={120}
+                width={120}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none w-max group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Wix</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/squarespace-logo.png"
+                className="relative z-10 w-[60px] aspect-auto m-auto duration-500 contrast-50 hover:contrast-100 hover:scale-125"
+                alt="Squarespace logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none w-max group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Squarespace</ThemedP>
+              </span>
+            </figure>
+            <figure className="relative group h-[60px] flex justify-center">
+              <Image
+                src="/images/skills/sanity-logo.png"
+                className="relative z-10 w-[45px] aspect-auto m-auto duration-500 saturate-[70%] hover:saturate-100 hover:scale-125"
+                alt="Sanity logo"
+                height={60}
+                width={60}
+              />
+              <span className="absolute font-bold duration-500 translate-x-1/2 opacity-0 select-none w-max group-hover:opacity-100 -top-8 right-1/2">
+                <ThemedP>Sanity</ThemedP>
+              </span>
+            </figure>
 
             {/* <SiAdobephotoshop className="relative z-10 w-[60px] h-[60px] m-auto duration-500 hover:text-[#31c5f0] border-[#31c5f0] rounded-xl hover:bg-white hover:border-8 border-spacing-2" /> */}
             {/* {SKILLS_LIST.map((skill) =>
@@ -206,7 +293,7 @@ export const SkillsSection = ({ language, Theme }) => {
                     alt={skill.alt}
                     src={skill.imageSrc}
                     className="relative z-10 m-auto duration-500 max-w-[70%]"
-                  ></Image>
+                  />
                 </div>
               ) : null
             )}
