d14f82f8d1beae5b5e515ab315c6b78eaf16d923
JoakimJobloop <Joakim@jobloop.no>
2024-01-26 15:24:37 +0100

    *

--- tailwind.config.js
+++ tailwind.config.js
@@ -212,6 +212,10 @@ module.exports = {
           "0%": { transform: "translateX(0)" },
           "100%": { transform: "translateX(-100%)" },
         },
+        reverseScroll: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
         fadeIn: {
           "0%": { opacity: "0" },
           "100%": { opacity: "100%" },
