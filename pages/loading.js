2000f6e309be48bb7d4dbef318092ff13372cfe1
Joakim Villo <villokodehode@gmail.com>
2024-03-25 09:56:00 +0100

    *

--- pages/loading.js
+++
@@ -1,6 +0,0 @@
-import PageLayout from "@/components/Layout/Layout";
-import { Loading } from "@/components/Utilities/LoadingScreen/LoadingScreen";
-
-export default function LoadingScreen() {
-  return <Loading />;
-}
